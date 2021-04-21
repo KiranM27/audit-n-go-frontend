a0e7c210322c0902a798ddb931ca198e41561c18
shuyijia <72814243+shuyijia@users.noreply.github.com>
2021-04-22 06:18:21 +0800

    update charts for tenant frontend

---
+++ src/components/dashboard/dashboardTenants/ChartsByTenant.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import {
+  CssBaseline,
+  Typography,
+  Paper,
+  Grid,
+  Container,
+  useMediaQuery,
+} from "@material-ui/core";
+import SynchronisedChart from "./SynchronisedCharts";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+const {
+  getAudits,
+  sortAudits,
+} = require("../../helperfunctions/AuditProcessing.js");
+
+export default function ChartsForTenant(props) {
+  const [auditData, setAuditData] = useState([]);
+  const [outletData, setOutletData] = useState([]);
+  const [instData, setInstData] = useState([]);
+  const themeTab = useTheme();
+  const isSmallScreen = useMediaQuery((theme) =>
+    themeTab.breakpoints.down("sm")
+  );
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  const retrieveData = async () => {
+    try {
+      const data = await axios.get(`/audits/0`).then((res) => {
+        console.log(res.data);
+        setAuditData(getAudits(res.data));
+      });
+      const outletData = await axios.get(`/outlets/0`).then((res) => {
+        const activeOutlets = [];
+        for (var i = 0; i < res.data.length; i++) {
+          if (res.data[i].active == true) {
+            activeOutlets.push(res.data[i]);
+          }
+        }
+        setOutletData(activeOutlets);
+      });
+
+      const institutionData = await axios
+        .get(`/getInstitutions`)
+        .then((res) => {
+          setInstData(res.data);
+        });
+    } catch (error) {
+      setAuditData([]);
+      setOutletData([]);
+      setInstData([]);
+    }
+  };
+  const chartProps = {
+    width: isSmallScreen ? 400 : 800,
+    height: isSmallScreen ? 200 : 300,
+  };
+
+  if (auditData.length != 0 && instData.length != 0 && outletData.length != 0) {
+    var data_barchart = genDataforCharts(auditData, instData, outletData);
+    var ncData = data_barchart[0];
+    var fbData = data_barchart[1];
+    var nfbData = data_barchart[2];
+  } else {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </div>
+    );
+  }
+  return (
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Grid>
+        <Typography
+          variant="subtitle2"
+          align="center"
+          style={{ textDecoration: "underline" }}
+        >
+          Non-Compliances
+        </Typography>
+        <Typography
+          variant="body2"
+          align="center"
+          style={{ fontStyle: "italic" }}
+        >
+          
+        </Typography>
+        <SynchronisedChart data={ncData} color="#DB324D"/>
+      </Grid>
+      <Grid>
+        <Typography
+          variant="subtitle2"
+          align="center"
+          style={{ textDecoration: "underline" }}
+        >
+          F&B Score
+        </Typography>
+        <Typography
+          variant="body2"
+          align="center"
+          style={{ fontStyle: "italic" }}
+        >
+          
+        </Typography>
+        <SynchronisedChart data={fbData} color="#36558F"/>
+      </Grid>
+      <Grid>
+        <Typography
+          variant="subtitle2"
+          align="center"
+          style={{ textDecoration: "underline" }}
+        >
+          Non F&B Score
+        </Typography>
+        <Typography
+          variant="body2"
+          align="center"
+          style={{ fontStyle: "italic" }}
+        >
+          
+        </Typography>
+        <SynchronisedChart data={nfbData} color="#F4B860"/>
+      </Grid>
+    </Grid>
+  );
+}
+function genDataforCharts(auditData, instData, outletData) {
+  var current_user_id = JSON.parse(Cookies.get("loggedInUser")).userId;
+  var filteredAuditData = sortAudits(auditData).filter(
+    (child) => child.outlet_id == current_user_id
+  );
+
+  var ncData = filteredAuditData
+    .filter((child) => child.type == "COVID-19")
+    .map((audit) => ({ name: audit.date, qty: audit.NC }));
+  var fbData = filteredAuditData
+    .filter((child) => child.type == "F&B")
+    .map((audit) => ({ name: audit.date, qty: audit.score }));
+  var nfbData = filteredAuditData
+    .filter((child) => child.type == "Non F&B")
+    .map((audit) => ({ name: audit.date, qty: audit.score }));
+
+  return [ncData.reverse(), fbData.reverse(), nfbData.reverse()];
+}
---
+++ src/components/dashboard/dashboardTenants/SynchronisedCharts.js
@@ -0,0 +1,53 @@
+import React, { PureComponent, useEffect, useState } from "react";
+import axios from "axios";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { Typography } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
+import Cookies from "js-cookie";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+export default function SynchronisedCharts(props) {
+  const themeTab = useTheme();
+  const isSmallScreen = useMediaQuery((theme) =>
+    themeTab.breakpoints.down("sm")
+  );
+
+  const chartProps = {
+    width: isSmallScreen ? 300 : 300,
+    height: isSmallScreen ? 200 : 200,
+  };
+
+  return (
+    <LineChart
+      {...chartProps}
+      data={props.data}
+      margin={{
+        top: 5,
+        right: 50,
+        left: 0,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" tick={{fontSize: 13}} />
+      <YAxis />
+      <Tooltip />
+      <Line
+        type="monotone"
+        dataKey="qty"
+        stroke={props.color}
+        strokeWidth={2.3}
+        activeDot={{ r: 8 }}
+      />
+    </LineChart>
+  );
+}
