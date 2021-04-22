4e9604c8a5517f1e3a81d92f09d3359ec8f1ccfa
Anirudh181001 <shrivijiani@gmail.com>
2021-04-23 00:21:40 +0800

    selenium

--- SeleniumTesting/FullFlow.java
+++ SeleniumTesting/FullFlow.java
@@ -2,9 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.Keys;
-
 public class FullFlow {
     public static void main(String[] args) throws InterruptedException {
 
@@ -12,10 +10,7 @@ public class FullFlow {
         String Password = "123456";
 
         // Change the link to the driver based on your local location
-        // Anirudh
-        // System.setProperty("webdriver.chrome.driver","C:/Users/shriv/Documents/ChromeDriver/chromedriver.exe");
-        // Kiran
-        System.setProperty("webdriver.chrome.driver","/Users/kiran/Desktop/SUTD/Misc/chromedriver");
+        System.setProperty("webdriver.chrome.driver","C:/Users/shriv/Documents/ChromeDriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
         // Ensure that the frontend and the backend of the app are running 
@@ -32,14 +27,16 @@ public class FullFlow {
         Thread.sleep(10000);
         driver.findElement(By.xpath("/html/body/div/div/div[1]/div[1]/div[4]/button")).click();
         Thread.sleep(2000);
+
+        //Navigate dashboard options
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div[2]/div/header/div/div/div/button[1]")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div[2]/div/header/div/div/div/button[2]")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div[2]/div/header/div/div/div/button[3]")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div[2]/div/header/div/div/div/button[4]")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         
         // View institutions
         driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/header/div/div[4]/button[2]")).click();
@@ -59,11 +56,76 @@ public class FullFlow {
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[1]/div/div/div/input")).sendKeys("CGH");
         Thread.sleep(1000);
-
-
-
-       
-
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[1]/div/div/div/input")).sendKeys(Keys.DOWN);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[1]/div/div/div/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[2]/div/div/div/div/button[2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[2]/div/div/div/input")).sendKeys("Dominos");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[2]/div/div/div/input")).sendKeys(Keys.DOWN);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[2]/div/div/div/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[3]/div/div/div/div/button[2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[3]/div/div/div/input")).sendKeys("C");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[3]/div/div/div/input")).sendKeys(Keys.DOWN);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[3]/div/div/div/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        //Select deadline
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[4]/div/div/input")).sendKeys("30/04/2021");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div/div/div[5]/div/div/button")).click();
+        Thread.sleep(1000);
+        //To expand audit options
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[3]/div[1]/div[1]/div[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[4]/button")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div[2]/div/header/div/div/div/button[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/main/div[3]/div/div[1]/div/div/p/div/div/div/div[2]/div[2]/div/div/div/div/div/div[1]/div[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div/label/span[1]/span[1]/span[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div/label/span[1]/span[1]/span[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div/label/span[1]/span[1]/span[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/header/div/div/div/button[2]")).click();
+        Thread.sleep(1000);
+        //Chat
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[4]/div/button")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[4]/div/div/form/input")).sendKeys("Hi I can type messages here :)");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[4]/div/div/form/button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[4]/div/button")).click();
+        Thread.sleep(1000);
+        //Delete the audit
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div/button[2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[2]")).click();
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept();
+        Thread.sleep(2000);
+        //Chatbot
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/a")).click();
+        Thread.sleep(2000);
+        //Manage Objects
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div[2]/ul/li[3]/button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div[4]/ul/li[3]/button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div[8]/ul/li[2]/button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[1]/a")).click();
+        Thread.sleep(1000);
+        
         driver.close();
     }
 }
