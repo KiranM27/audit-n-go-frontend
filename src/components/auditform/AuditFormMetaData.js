adb782f89993fc7e7842fef6c1d405894251587b
KiranMohan27 <58102994+KiranM27@users.noreply.github.com>
2021-04-22 20:40:57 +0800

    Compute score for diff types of audits

--- src/components/auditform/AuditFormMetaData.js
+++ src/components/auditform/AuditFormMetaData.js
@@ -1,61 +1,76 @@
 const metaData = {
-    'cv': 
-    {
-        'partsNo': 2,
-        'partDetails': [
-            {
-            'partNo': 1,
-            'partTitle': "Safe Management Measures for Front-of-house"
-            }, 
-            {
-                'partNo': 2,
-                'partTitle': "Staff Hygiene & Safe Management Measures"
-            }]
-    },
-    'fb': 
-    {
-        'partsNo': 5,
-        'partDetails': [
-            {
-            'partNo': 1,
-            'partTitle': "Professionalism & Staff Hygiene"
-            }, 
-            {
-                'partNo': 2,
-                'partTitle': "Housekeeping & General Cleanliness"
-            },
-            {
-                'partNo': 3,
-                'partTitle': "Food Hygiene"
-            },
-            {
-                'partNo': 4,
-                'partTitle': "Healthier Choice in line with HPB’s Healthy Eating’s Initiative"
-            },
-            {
-                'partNo': 5,
-                'partTitle': "Workplace Safety & Health"
-            }
-        ]
-    }, 
-    'nf': 
-    {
-        'partsNo': 3,
-        'partDetails': [
-            {
-                'partNo': 1,
-                'partTitle': "Professionalism & Staff Hygiene"
-            }, 
-            {
-                'partNo': 2,
-                'partTitle': "Housekeeping & General Cleanliness"
-            },
-            {
-                'partNo': 3,
-                'partTitle': "Workplace Safety & Health"
-            }
-        ]
-    },
-}
+  cv: {
+    partsNo: 2,
+    partDetails: [
+      {
+        partNo: 1,
+        partTitle: "Safe Management Measures for Front-of-house",
+      },
+      {
+        partNo: 2,
+        partTitle: "Staff Hygiene & Safe Management Measures",
+      },
+    ],
+  },
+  fb: {
+    partsNo: 5,
+    partDetails: [
+      {
+        partNo: 1,
+        partTitle: "Professionalism & Staff Hygiene",
+        maxScore: 10,
+        noItems: 13
+      },
+      {
+        partNo: 2,
+        partTitle: "Housekeeping & General Cleanliness",
+        maxScore: 20,
+        noItems: 17
+      },
+      {
+        partNo: 3,
+        partTitle: "Food Hygiene",
+        maxScore: 35,
+        noItems: 37
+      },
+      {
+        partNo: 4,
+        partTitle:
+          "Healthier Choice in line with HPB’s Healthy Eating’s Initiative",
+        maxScore: 15,
+        noItems: 11
+      },
+      {
+        partNo: 5,
+        partTitle: "Workplace Safety & Health",
+        maxScore: 20,
+        noItems: 18
+      },
+    ],
+  },
+  nf: {
+    partsNo: 3,
+    partDetails: [
+      {
+        partNo: 1,
+        partTitle: "Professionalism & Staff Hygiene",
+        maxScore: 20,
+        noItems: 9
+      },
+      {
+        partNo: 2,
+        partTitle: "Housekeeping & General Cleanliness",
+        maxScore: 40,
+        noItems: 12
+      },
+      {
+        partNo: 3,
+        partTitle: "Workplace Safety & Health",
+        maxScore: 40,
+        noItems: 16
+      },
+    ],
+  },
+};
 
-export default metaData
+export default metaData;
